910a918993681f440b672c44c6168b1e6340f46e
Rezaul Karim <rezaul.km105@gmail.com>
2023-03-30 21:31:31 +0200

    init commit

--- backend/SendEmail.py
+++ backend/SendEmail.py
@@ -21,8 +21,6 @@ class SendEmail(object):
         print('Email host', settings.SMTP_HOST, settings.SMTP_PORT, settings.SMTP_USER)
         with smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT, context=context) as server:
         #with smtplib.SMTP(settings.SMTP_HOST, settings.SMTP_PORT) as server:
-            server.starttls()
-            server.ehlo()
             server.login(settings.SMTP_USER,settings.SMTP_PASS)
             msg = MIMEMultipart('related')
             msg['Subject'] = 'দারুল ইহসান নোটিশ বোর্ড সাইনআপের আমন্ত্রণ'
