64a2094d6f56af0789d1b62029800c7ae68b8979
Rezaul Karim <rezaul.km105@gmail.com>
2023-03-30 17:52:53 +0200

    init commit

--- frontend/static/ckeditor/ckeditor/styles.js
+++ frontend/static/ckeditor/ckeditor/styles.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
  * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
  */
@@ -134,3 +134,4 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: '1080p ', type: 'widget', widget: 'embed', attributes: { 'class': 'embed-1080p' }, group: 'size' }
 
 ] );
+
