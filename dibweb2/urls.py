c8d2492b26728183f94224de4897b2034f5f8153
Rezaul Karim <rezaul.km105@gmail.com>
2024-09-16 13:57:35 +0200

    add contact details

--- dibweb2/urls.py
+++ dibweb2/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('backend.routers')),
     # path('api/', include('backend.urls')),
     path('', include('frontend.urls'))
 ]
