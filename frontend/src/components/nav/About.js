3d617d2ba12eb970806925177d9bd3dfd10d2350
Rezaul Karim <rezaul.km105@gmail.com>
2024-10-10 20:46:34 +0200

    Modify styling

--- frontend/src/components/nav/About.js
+++ frontend/src/components/nav/About.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
+import {  useLocation } from "react-router-dom";
 
 export default function About() {
     const { t } = useTranslation();
@@ -8,13 +9,13 @@ export default function About() {
     return (
         <Fragment>
             <div className="nav-item dropdown">
-                <a href="#" className="nav-link" data-bs-toggle="dropdown">
+                <a href="#" className={`nav-link ${location.pathname === "#" ? "active" : ""}`} data-bs-toggle="dropdown">
                     <span className="dropdown-toggle">{t('AboutUs.text')}</span>
                 </a>
                 <div className="dropdown-menu m-0">
-                    <Link to="/history" className="dropdown-item">{t('OurHistory.text')}</Link>
-                    <Link to="/goalsobjective" className="dropdown-item">{t('GoalsAndObjectives.text')}</Link>
-                    <Link to="/idealsvalue" className="dropdown-item">{t('IdealsAndValues.text')}</Link>
+                    <Link to="/history" className={`dropdown-item ${location.pathname === "/history" ? "active" : ""}`} >{t('OurHistory.text')}</Link>
+                    <Link to="/goalsobjective" className={`dropdown-item ${location.pathname === "/goalsobjective" ? "active" : ""}`} >{t('GoalsAndObjectives.text')}</Link>
+                    <Link to="/idealsvalue" className={`dropdown-item ${location.pathname === "/idealsvalue" ? "active" : ""}`} >{t('IdealsAndValues.text')}</Link>
                 </div>
             </div>
         </Fragment>
--- frontend/src/components/nav/Navbar.js
+++ frontend/src/components/nav/Navbar.js
@@ -1,29 +1,32 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useState} from "react";
 import {Link} from "react-router-dom";
 import Language from "./Language";
 import { useTranslation } from 'react-i18next';
 import About from "./About";
+import {  useLocation } from "react-router-dom";
 
 export default function Navbar(){
     const { t } = useTranslation();
+
     return(
             <Fragment>
                 {/* Navbar & Hero Start */}
                 <nav className="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
                     <a href="/" className="navbar-brand p-0">
-                        <h2 className="text-primary">  <img src={'/static/assets/images/dib-logo-new.png'} alt="Logo"/></h2>
+                        <h2 className="text-primary">  <img src={'/static/assets/images/dib-logo-new.png'} alt="Logo"/> &nbsp;Darul Ihsan Berlin e.V</h2>
                     </a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
                         <span className="fa fa-bars"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarCollapse">
                         <div className="navbar-nav ms-auto py-0">
-                            <a href={'/'} className={"nav-item nav-link active"}>{t('Home.text')}</a>
-                            <About />
-                            <Link to={"/activities/"} className="nav-item nav-link">{t('Activities.text')}</Link>
-                            <Link to={"/donation/"} className="nav-item nav-link">Donation</Link>
-                            <Link to={"/prayer-time/"} className="nav-item nav-link">Prayer Time</Link>
-                            <Link to={"/contact/"} className="nav-item nav-link">{t('ContactUs.text')}</Link>
+                            <a href={'/'} className={`nav-item nav-link ${location.pathname === "/" ? "active" : ""}`}>{t('Home.text')}
+                            </a>
+                            <About/>
+                            <Link to={"/activities/"} className={`nav-item nav-link ${location.pathname === "/activities/" ? "active" : ""}`}>{t('Activities.text')}</Link>
+                            <Link to={"/donation/"} className={`nav-item nav-link ${location.pathname === "/donation/" ? "active" : ""}`}>{t('Donation.text')}</Link>
+                            <Link to={"/prayer-time/"} className={`nav-item nav-link ${location.pathname === "/prayer-time/" ? "active" : ""}`}>{t('PrayerTime.text')}</Link>
+                            <Link to={"/contact/"} className={`nav-item nav-link ${location.pathname === "/contact/" ? "active" : ""}`}>{t('ContactUs.text')}</Link>
                             <Language/>
                         </div>
                     </div>
