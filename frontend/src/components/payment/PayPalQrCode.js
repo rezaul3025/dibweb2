c82f0800999ff387c2d7c74e3fe0a2efd2d2eabd
Rezaul Karim <rezaul.km105@gmail.com>
2024-11-12 12:22:10 +0100

    add chages on home page

--- frontend/src/components/payment/PayPalQrCode.js
+++ frontend/src/components/payment/PayPalQrCode.js
@@ -4,10 +4,10 @@ export default function PayPalQrCode() {
     return (
         <div className="p-4 bg-light" style={{maxWidth: '800px'}}>
             <div className="col-md-12 col-lg-12 col-xl-12">
-                <div className="text-center p-4 bg-light">
+                <div className="text-center bg-light">
                     <p> Scan QR-Code to pay via PayPal</p>
-                    <img className="align-content-center" src="/static/assets/img/paypal/pay-qr.jpg"
-                         alt="PayPal Pay QR code" width="300" height="300"/>
+                    <img className="text-center" src="/static/assets/img/paypal/pay-qr.jpg"
+                         alt="PayPal Pay QR code" width="230" height="230"/>
                 </div>
             </div>
         </div>
