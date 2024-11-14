150f4af26f9155a4225ba50702666b8f0b571b2f
Rezaul Karim <rezaul.km105@gmail.com>
2024-11-14 12:36:03 +0100

    add chages on home page

--- frontend/src/components/event/Event.js
+++ frontend/src/components/event/Event.js
@@ -69,7 +69,8 @@ export default function Event() {
                                                 className="text-primary fas fa-solid fa-euro-sign"></i><span
                                                 className="text-primary">10</span></li>
                                             <li className="list-group-item">
-                                                <small>Reference for both bank and payal : <b>MDI_Ahmadullah Event Ticket</b></small>
+                                                <small>Reference for both bank and payal : <b>MDI_Ahmadullah Event
+                                                    Ticket</b></small>
                                             </li>
                                         </ul>
                                         <BankDetails/>
@@ -77,15 +78,69 @@ export default function Event() {
                                         <PayPalQrCode/>
                                     </div>
                                     {//event.enabled && event.attendee_limit > event.attendee_count &&
-                                      //  <div className="align-bottom p-3">
-                                      //      <Link to={"/registration/" + event.id + "/"}
-                                       //           className="btn btn-primary align-bottom">Buy With PayPal
-                                       //     >></Link>
-                                    //</div>
-                                        }
+                                        //  <div className="align-bottom p-3">
+                                        //      <Link to={"/registration/" + event.id + "/"}
+                                        //           className="btn btn-primary align-bottom">Buy With PayPal
+                                        //     >></Link>
+                                        //</div>
+                                    }
                                 </div>}
                             </div>
                         ))}
+
+                    </div>
+                    <div className="text-center mx-auto p-4 wow fadeInUp" data-wow-delay="0.2s"
+                         style={{maxWidth: '800px'}}>
+                        <h6 className="mb-0">All previous event(s)</h6>
+                    </div>
+                    <div className="row g-4">
+                        {loading && <Spinner width="6rem" height="6rem"/>}
+                        {events != null && events.map((event) => (
+                            <div className="col-md-12 col-lg-12 col-xl-12 wow fadeInUp d-flex align-items-stretch"
+                                 key={event.id}
+                                 data-wow-delay="0.2s">
+                                {!event.enabled && <div className="card">
+                                    <img src={'/static/assets' + event.poster_image} className="card-img-top"
+                                         alt={event.poster_image}/>
+                                    <div className="card-body">
+                                        <h5 className="card-title">{event.title}</h5>
+                                        <p className="card-text ">
+                                            {event.description}
+                                        </p>
+                                        <h6><i
+                                            className="fas fa-solid fa-location-crosshairs  text-primary me-2"></i>{event.place}
+                                        </h6>
+                                        <h6 className="pt-2"><i
+                                            className="fas fa-map-marker-alt text-primary me-2"></i>{event.address}
+                                        </h6>
+                                        <h6 className="py-2"><i
+                                            className="fas fa-regular fa-clock text-primary me-2"></i> {moment(event.event_datetime).format("LLL")}
+                                        </h6>
+
+                                        <a href={event.map_location} target="_blank"><i
+                                            className="fa-solid fa-map-location text-primary me-2"></i> Signup
+                                        </a>
+                                        <ul className="list-group list-group-flush pt-2">
+                                            <li className="list-group-item"><small>Ticket price:</small></li>
+                                            <li className="list-group-item"><small>Adult </small><i
+                                                className="text-primary fas fa-solid fa-euro-sign"></i><span
+                                                className="text-primary">20</span></li>
+                                            <li className="list-group-item"><small>Children from age 5 to 17 </small><i
+                                                className="text-primary fas fa-solid fa-euro-sign"></i><span
+                                                className="text-primary">10</span></li>
+                                        </ul>
+                                    </div>
+                                    {//event.enabled && event.attendee_limit > event.attendee_count &&
+                                        //  <div className="align-bottom p-3">
+                                        //      <Link to={"/registration/" + event.id + "/"}
+                                        //           className="btn btn-primary align-bottom">Buy With PayPal
+                                        //     >></Link>
+                                        //</div>
+                                    }
+                                </div>}
+                            </div>
+                        ))}
+
                     </div>
                 </div>
             </div>
