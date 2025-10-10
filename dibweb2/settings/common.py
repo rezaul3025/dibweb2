90aea32ba8e5aaa82ee9ef31484019035f9b38d0
Rezaul Karim <rezaul.km105@gmail.com>
2025-10-10 15:41:21 +0200

    test herz deployment

--- dibweb2/settings/common.py
+++ dibweb2/settings/common.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 CORS_ORIGIN_ALLOW_ALL = True
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'prayer.darulihsan-berlin.com','darulihsan-berlin.com', 'master', 'http://0.0.0.0:8089/']
+ALLOWED_HOSTS = ['static.21.135.99.91.clients.your-server.de','127.0.0.1', 'localhost', 'prayer.darulihsan-berlin.com','darulihsan-berlin.com', 'master', 'http://0.0.0.0:8089/']
 
 # Application definition
 
